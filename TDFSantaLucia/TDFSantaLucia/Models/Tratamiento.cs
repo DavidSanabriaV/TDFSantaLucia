82e71e9b7b50b19b70bd5e86804da27ba98ad33e
DavidG <davidgonzalezlda497@gmail.com>
2026-05-15 00:58:11 -0600

    User Model and Treatment

--- TDFSantaLucia/TDFSantaLucia/Models/Tratamiento.cs
+++ TDFSantaLucia/TDFSantaLucia/Models/Tratamiento.cs
@@ -1,6 +1,29 @@
-﻿namespace TDFSantaLucia.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TDFSantaLucia.Models
 {
     public class Tratamiento
     {
+        [Key]
+        public int Tratamiento_Id { get; set; }
+
+        public string? Nombre_Medicamento { get; set; }
+
+        public string? Dosis { get; set; }
+
+        public string? Duracion { get; set; }
+
+        public DateTime Fecha_Inicio { get; set; }
+
+        public DateTime Fecha_Fin { get; set; }
+
+        public bool Estado { get; set; }
+
+        [ForeignKey(nameof(Cliente))]
+        public int Cliente_Id { get; set; }
+        public Cliente? Cliente { get; set; }
+
+        public List<RecordatorioTratamiento> Recordatorios { get; set; } = new();
     }
 }
--- TDFSantaLucia/TDFSantaLucia/Models/Usuario.cs
+++ TDFSantaLucia/TDFSantaLucia/Models/Usuario.cs
@@ -1,6 +1,31 @@
-﻿namespace TDFSantaLucia.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace TDFSantaLucia.Models
 {
-    public class Usuario
+    public class Usuario : IdentityUser
     {
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required]
+        public string Primer_Apellido { get; set; }
+
+        [Required]
+        public string Segundo_Apellido { get; set; }
+
+        [Required]
+        public bool Estado { get; set; } = true;
+
+        public string? Direccion_Exacta { get; set; }
+
+        public string? Telefono { get; set; }
+
+        [NotMapped]
+        public string? RolNombre { get; set; }
+
+        public Empleado? Empleado { get; set; }
+        public Cliente? Cliente { get; set; }
     }
 }
