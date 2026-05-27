bee696c45a46103d5d335a8fe009d3ea5477f20b
josefgonzalezz <jgonzalez50082@ufide.ac.cr>
2026-05-27 00:51:03 -0600

    Index
    Index

--- TDFSantaLucia/TDFSantaLucia/Controllers/HomeController.cs
+++ TDFSantaLucia/TDFSantaLucia/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using TDFSantaLucia.Data;
 using TDFSantaLucia.Models;
 
 namespace TDFSantaLucia.Controllers
@@ -7,14 +9,22 @@ namespace TDFSantaLucia.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            var productos = _context.Productos
+                .Include(p => p.Categoria)
+                .Take(20)
+                .ToList();
+
+            ViewBag.Productos = productos;
             return View();
         }
 
